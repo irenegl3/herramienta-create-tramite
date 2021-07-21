3af238629879944b322794c23f8fa9c60721a97a
IRENE <irene.garcialop@alumnos.upm.es>
2021-07-21 23:12:04 +0200

    cleaned

--- create-tramite/userCode/back/db/seeders/20200914100531-Permisos.js
+++ create-tramite/userCode/back/db/seeders/20200914100531-Permisos.js
@@ -6,52 +6,12 @@ module.exports = {
     try {
       await queryInterface.bulkInsert('Permisos', [
         {
-          email: 'elena.garcia.leal@upm.es',
+          email: 'tes1@upm.es',
           tramite: 'gestion-certificados'
         }, 
         {
-          email: 'andres.cosano@upm.es',
-          tramite: 'gestion-certificados'
-        },
-        {
-          email: 'elena.garcia.leal@upm.es',
+          email: 'test2@upm.es',
           tramite: 'gestion-titulos'
-        }, 
-        {
-          email: 'andres.cosano@upm.es',
-          tramite: 'gestion-titulos'
-        },
-        {
-          email: 'mariaantonia.barquero@upm.es',
-          tramite: 'gestion-titulos'
-        },
-        {
-          email: 'cristina.perezdeazpillaga@upm.es',
-          tramite: 'gestion-titulos'
-        },
-        {
-          email: 'pilar.horcajada@upm.es',
-          tramite: 'gestion-titulos'
-        },
-        {
-          email: 'esther.fuentes@upm.es',
-          tramite: 'gestion-titulos'
-        },
-        {
-          email: 'mercedes.obispo@upm.es',
-          tramite: 'gestion-titulos'
-        },
-        {
-          email: 'isabel.poza@upm.es',
-          tramite: 'gestion-titulos'
-        },
-        {
-          email: 'elena.garcia.leal@upm.es',
-          tramite: 'evaluacion-curricular'
-        }, 
-        {
-          email: 'andres.cosano@upm.es',
-          tramite: 'evaluacion-curricular'
         },
       ]);
     } catch (error) {
